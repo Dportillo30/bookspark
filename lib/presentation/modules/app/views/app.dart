bc4a980fec3c757c46aeb0cf99cbe343d7ecd070
Danilo Portillo <dportillodev30@gmail.com>
2023-05-11 15:58:18 -0600

    add app_bloc

--- lib/presentation/modules/app/views/app.dart
+++ lib/presentation/modules/app/views/app.dart
@@ -1,27 +1,44 @@
-import "package:flutter/material.dart";
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../config/themes/themes/app_theme.dart';
+import '../../../../domain/repository/authentication_repository.dart';
+import '../controllers/bloc/app_bloc.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({
+    super.key,
+    required AuthenticationRepository authenticationRepository,
+  }) : _authenticationRepository = authenticationRepository;
+
+  final AuthenticationRepository _authenticationRepository;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AppBloc(
+          authenticationRepository: _authenticationRepository,
         ),
+        child: const AppView(),
       ),
     );
   }
 }
 
-
 class AppView extends StatelessWidget {
-const AppView({ Key? key }) : super(key: key);
+  const AppView({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      home: FlowBuilder<AppStatus>(
+        state: context.select((AppBloc bloc) => bloc.state.status),
+        onGeneratePages: onGenerateAppViewPages,
+      ),
+    );
   }
 }
